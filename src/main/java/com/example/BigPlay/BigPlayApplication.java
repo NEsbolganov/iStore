45d552b30b75602c30cb993c1775c396075b4b79
Nartay <esbolganovnartai@gmail.com>
2023-05-14 12:13:01 +0600

    starting project

---
+++ src/main/java/com/example/BigPlay/BigPlayApplication.java
@@ -0,0 +1,13 @@
+package com.example.BigPlay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigPlayApplication.class, args);
+	}
+
+}
